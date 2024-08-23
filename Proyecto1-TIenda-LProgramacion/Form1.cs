3d7065e027239fb6011887cf94a7c408063e5d56
JeSuScS12 <chaconsotojesus4@gmail.com>
2024-08-23 17:19:00 -0300

    Agregar archivos de proyecto.

---
+++ Proyecto1-TIenda-LProgramacion/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto1_TIenda_LProgramacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        conexion nuevo = new conexion();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nuevo.Conect();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Proyecto1-TIenda-LProgramacion/conexion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace Proyecto1_TIenda_LProgramacion
+{
+    internal class conexion
+    {
+        OleDbCommand comando;
+        OleDbConnection conectar;
+        OleDbDataAdapter adaptador;
+
+        string cadena;
+
+        public conexion()
+        {
+            cadena = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=./Pyt1-Tienda.accdb";
+        }
+
+        public void Conect()
+        {
+            try
+            {
+                conectar = new OleDbConnection(cadena);
+                conectar.Open();
+                MessageBox.Show("Se conecto a BD");
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("Error" + x, "😒");
+            }
+        }
+
+        public void LlenarTabla()
+        {
+            try
+            {
+
+
+
+
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show("Error"+error);
+            }
+        }
+
+    }
+}
